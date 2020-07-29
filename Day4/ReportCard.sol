daed1fe07b26015f8869482041e344c67eb119b4
Sahil Rana <51770809+Rsr0@users.noreply.github.com>
2020-07-29 10:49:12 +0530

    Update ReportCard.sol

--- Day4/ReportCard.sol
+++ Day4/ReportCard.sol
@@ -4,28 +4,28 @@ contract ReportCard{
     
     string public Name;
     uint public   RollNo ;
-    uint public Batch;
-   uint sub1;
+    string public Batch;
+    uint sub1;
     uint sub2;
     uint sub3;
     uint sub4;
     string public Status;
-    
-    function ReportCard(string newName, uint newRollNo, uint newBatch,uint newsub1,uint newsub2,uint newsub3,uint newsub4,string newStatus) public{
-        
-    Name = newName;
-    RollNo = newRollNo;
-    Batch = newBatch;
-    sub1=newsub1;
-    sub2=newsub2;
-    sub3=newsub3;
-    sub4=newsub4;
-    Status=newStatus;
-  
-    }
+
+    function ReportCard(string newName, uint newRollNo, string newBatch,uint newsub1,uint newsub2,uint newsub3,uint newsub4) public{
+        Name = newName;
+        RollNo = newRollNo;
+        Batch = newBatch;
+        sub1=newsub1;
+        sub2=newsub2;
+        sub3=newsub3;
+        sub4=newsub4;
+      
+        (sub1>33&&sub2>33&&sub3>33&&sub4>33) ? Status="Pass" : Status="Fail";
+   }
     
     function getDetails() public view returns(string,uint,uint,uint,uint,uint,uint,string){
-        return(Name,RollNo,Batch,sub1,sub2,sub3,sub4, Status);
+       
+       return(Name,RollNo,Batch,sub1,sub2,sub3,sub4, Status);
     }
     
     
